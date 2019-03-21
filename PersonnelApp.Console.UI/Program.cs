bb1321481dcf71d62f643b8bea228be9f99ecfc3
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-21 22:15:33 +0300

    Tüm database işlemleri UnitOfWork classı sayesinde tek bir yerden yönetildi.

---
+++ PersonnelApp.Console.UI/Program.cs
@@ -0,0 +1,31 @@
+﻿using PersonnelApp.DAL;
+using PersonnelApp.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelApp.Console.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitOfWork unitOfWork = new UnitOfWork(new PersonnelContext());
+            unitOfWork.DepartmentRepository.Add(new Department()
+            {
+                Name = "Muhasebe",
+                IsActive = true,
+                CreatedDate = DateTime.Now
+            });
+            unitOfWork.DepartmentRepository.Add(new Department()
+            {
+                Name = "Pazarlama",
+                IsActive = true,
+                CreatedDate = DateTime.Now
+            });
+            unitOfWork.Complete();
+        }
+    }
+}
